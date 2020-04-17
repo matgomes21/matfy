3e28862dd82d1248b8d1def31c47c9c193c6b2f3
Mat <mateusgomes.nascimento21@gmail.com>
2020-04-16 21:11:56 -0300

    Added relations with models and validations

--- db/migrate/20200417000443_create_songs.rb
+++ db/migrate/20200417000443_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[6.0]
   def change
     create_table :songs do |t|
       t.string :title
-      t.integer :played_count
+      t.integer :played_count, default: 0
       t.references :album, null: false, foreign_key: true
 
       t.timestamps
